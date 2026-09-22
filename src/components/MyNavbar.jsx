aacb9ac9d415ac4c28da48bcda789a77e8b508c8
Illya Savitskyi <ilyukha250111@gmail.com>
2026-09-22 20:43:25 +0300

    Update project

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,17 +1,73 @@
-import React, { UseState, useEffcet } from 'react'
-import { Navbar, Nav, Container, Button, Badge } from 'react-bootstrap'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import Parse from '../services/parse'
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav, Container, Button, Badge } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Parse from '../services/parse';
 
 export default function MyNavbar() {
+  
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  return (
-    <div className="navbar">
-      <Navbar>
+    const [currentUser, setCurrentUser] = useState(null);
+
+      // OnStart:
+      useEffect(() => {
+        const user = Parse.User.current();
+        setCurrentUser(user);
+      }, [])
+
+
+      async function handleLogout() {
+        try {
+            await Parse.User.logOut();
+            navigate('/login')
+        } catch (err) {
+            console.error(err);
+            alert('Помилка виходу: ' + err.message);
+        }
+    }
+
+
+    
+    return (
+    <Navbar bg='dark' variant='dark' expand='lg' className='shadow-sm'>
         <Container>
-          Navbar
+            <Navbar.Brand as={Link} to='/' className='fw-bold fs-4' style={{
+                color: 'bisque', fontFamily: 'Trebuchet MS'
+            }}>
+                HR-App
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto'>
+                <Nav.Link as={Link} to='/' active={location.pathname === '/'}>
+                    Головна
+                </Nav.Link>
+                <Nav.Link as={Link} to='/employees' active={location.pathname === '/employees'}>
+                    Співробітники
+                </Nav.Link>
+                <Nav.Link as={Link} to='/departments' active={location.pathname === '/departments'}>
+                    Депортаменти
+                </Nav.Link>
+                <Nav.Link as={Link} to='/reports' active={location.pathname === '/reports'}>
+                    Звіти
+                </Nav.Link>
+              </Nav>
+              <Nav className='d-flex align-item-center gep-3'>
+                {currentUser && (
+                   <div className='text-light d-flex align-items-center gap-2'>
+                    {currentUser.get('username') || currentUser.get('email')}
+                    <Badge bg='secondary' className='ms-2'>HR</Badge>
+                   </div>
+
+                )}
+                <Button variant='outline-light' size='sm' onClick={handleLogout}>
+                  вийти
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
         </Container>
-      </Navbar>
-    </div>
-  )
+    </Navbar>
+)
+
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Form, Button, Card, FormControl } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import Parse from '../services/parse';
 
-export default function login() {
-  return (
-    <div>login</div>
-  )
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleLogin(event) {
+        event.preventDefault();
+
+        try {
+            await Parse.User.logIn(username, password);
+            navigate('/');
+        } catch (err) {
+            alert('Невірний логін або пароль');
+        }
+    }
+
+    return (
+        <Container
+            className='d-flex justify-content-center align-items-center'
+            style={{ minHeight: '100vh' }}
+        >
+            <Card style={{ width: '400px' }}>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Вхід до системи</h2>
+
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Логін:</Form.Label>
+                            <FormControl
+                                value={username}
+                                onChange={event => setUsername(event.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Пароль:</Form.Label>
+                            <FormControl
+                                type='password'
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Button
+                            type='submit'
+                            variant='primary'
+                            className='w-100'
+                        >
+                            Увійти
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 }
